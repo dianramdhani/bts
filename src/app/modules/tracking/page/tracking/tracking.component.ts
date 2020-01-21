33f3882ce983d387138c32078d81b666493a6240
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-01-21 17:19:08 +0700

    init tracking

--- src/app/modules/tracking/page/tracking/tracking.component.ts
+++ src/app/modules/tracking/page/tracking/tracking.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-tracking',
   templateUrl: './tracking.component.html',
   styleUrls: ['./tracking.component.scss']
 })
-export class TrackingComponent implements OnInit {
+export class TrackingComponent implements OnInit, AfterViewInit {
+  scriptElement = document.createElement('script');
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.scriptElement.src = './dashforge.profile.js';
+    document.body.appendChild(this.scriptElement);
+  }
+
+  ngOnDestroy() {
+    this.scriptElement.parentElement.removeChild(this.scriptElement);
   }
 
 }
