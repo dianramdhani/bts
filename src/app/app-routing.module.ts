c8f6b369626dfc3bbe1b44da33f1eedf3890165f
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-01-21 16:24:08 +0700

    init tracking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tracking',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: [
+      { path: 'tracking', loadChildren: () => import('@modules/tracking/tracking.module').then(m => m.TrackingModule) }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
